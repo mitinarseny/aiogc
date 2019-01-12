8a1df1ccd0d81f616799aec0efdd64454ba6dfcb
Arseny Mitin <mitinarseny@gmail.com>
2019-01-13 01:12:01 +0300

    fix: version, imports

--- aiogc/__init__.py
+++ aiogc/__init__.py
@@ -1,8 +1,7 @@
 GOOGLEAPIS_BASE_URL = 'https://www.googleapis.com'
 GOOGLE_TOKEN_URI = 'https://oauth2.googleapis.com/token'
 
-name = "aiogc"
-__version__ = '0.1.1'
+__name__ = "aiogc"
 
 from . import events
 from .models import Credentials, Event, Time, Person
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aiogc",
-    version="0.1.2",
+    version="0.1.3",
     author="Arseny Mitin",
     author_email="mitinarseny@gmail.com",
     description="Async Google Calendar API Client",
